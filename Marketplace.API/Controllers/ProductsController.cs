029c5610e1c1dea9d78b1da53a2419cc45ab88f2
Sevgin Ibryam <skate@LAPTOP-FGP3J5BF>
2022-09-26 12:56:11 +0300

    Added product controller

---
+++ Marketplace.API/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Marketplace.API.Controllers
+{
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController()
+        {
+
+        }
+
+        public JsonResult GetProducts()
+        {
+            var products = // getProducts();
+
+            return new JsonResult({
+                products
+            });
+        }
+    }
+}
